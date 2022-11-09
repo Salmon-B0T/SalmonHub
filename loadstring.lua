0f688a573edbf085b9c1d9efbc82bc7bb3058ae6
Salmon <115488473+Salmon-B0T@users.noreply.github.com>
2022-11-09 09:42:17 +0200

    Update loadstring.lua

--- loadstring.lua
+++ loadstring.lua
@@ -1,1 +1,2 @@
+--this literally just exists so u dont have to copy the script every time i update
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Salmon-B0T/SalmonHub/main/SalmonHub.lua", true))()
