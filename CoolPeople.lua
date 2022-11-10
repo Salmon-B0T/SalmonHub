f8dbf3f92f0c1d6cdf2d5464fbf83441f9333f1b
Salmon <115488473+Salmon-B0T@users.noreply.github.com>
2022-11-10 09:53:18 +0200

    cool people :thumbsup:

---
+++ CoolPeople.lua
@@ -0,0 +1,14 @@
+--Player tags, this just exists so that people who use the script can see certain people
+--The pastebins are unlisted but still readable
+    
+    --Somn1ph0bia
+    loadstring(game:HttpGet("https://pastebin.com/raw/M4vHABZN", true))()
+    
+    --iswattable
+    loadstring(game:HttpGet("https://pastebin.com/raw/F1w3f2CQ", true))()
+    
+    --FalseValues
+     loadstring(game:HttpGet("https://pastebin.com/raw/cQnk83gM", true))()
+
+    --Somn1um
+    loadstring(game:HttpGet("https://pastebin.com/raw/zwuLQSET", true))()
